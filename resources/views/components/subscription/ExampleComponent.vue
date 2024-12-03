87b1a38f9e2e64bccdc8055be33315679ca58c05
Jeevan Oraganti <jeevanoraganti75@gmail.com>
2024-12-03 09:54:53 +0530

    Add ExampleComponent for subscription feature with input binding

---
+++ resources/views/components/subscription/ExampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input type="text" v-model="message">
+        <p>The value of the input is: {{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello World'
+        };
+    }
+};
+</script>
